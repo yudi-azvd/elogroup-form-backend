1cb448f29635089865e76ff1dd202d38d2fe3d47
Yudi Yamane <yudiyamane@gmail.com>
2020-03-04 12:38:26 -0300

    test to deploy

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+require('dotenv/config')
+
 const express = require('express')
 const cors = require('cors')
 
@@ -7,8 +9,12 @@ app.use(express.json())
 app.use(cors())
 
 app.post('/', async (req, res) => {
-  console.log(req.body)
   return res.json({ ok: true })
 })
 
-app.listen(8080)
+
+const PORT = process.env.PORT  || 8080
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`)
+})
